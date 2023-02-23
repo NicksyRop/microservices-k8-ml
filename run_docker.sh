dd4e8c38acd2c6274434be99856cc43a5fe13185
NicksyRop <nicksonkipkorir25@gmail.com>
2023-02-23 20:57:40 +0300

    updated the run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -4,7 +4,7 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build --tag=udoyen/skylearnmlproject .
+docker build --tag=nicksyrop/flaskmlproject .
 
 # Step 2: 
 # List docker images
@@ -12,4 +12,4 @@ docker image ls
 
 # Step 3: 
 # Run flask app
-docker run -p 8000:80 udoyen/skylearnmlproject
+docker run -p 8000:80 nicksyrop/flaskmlproject
